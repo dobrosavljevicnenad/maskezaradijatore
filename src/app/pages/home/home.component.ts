46a276f71fc0bd7dea6d9c183e87f2be870de7c3
Nenad Dobrosavljevic <n.dobrosavljevic01@gmail.com>
2026-04-09 12:53:22 +0200

    add index.html and new home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  title = 'Maske za radijatore';
-  subtitle = 'Elegantne i moderne maske za radijatore po meri vašeg prostora.';
+  phoneHref = 'tel:+381659775995';
+  phoneDisplay = '065 977 5995';
+
+  product = {
+    naziv: 'Dekorativna maska za radijator',
+    slug: 'maska-za-radijator',
+    opis:
+      'Metalna maska za radijator od plastificiranog lima, moderna zaštita i elegantno rešenje za lepši enterijer.',
+    cena: 12480,
+    popust: 0,
+    novo: true,
+    slika: [
+      {
+        url: 'assets/maska-za-radijator.png'
+      }
+    ]
+  };
+
+  get priceFormatted(): string {
+    return this.product.cena.toLocaleString('sr-RS');
+  }
 }
